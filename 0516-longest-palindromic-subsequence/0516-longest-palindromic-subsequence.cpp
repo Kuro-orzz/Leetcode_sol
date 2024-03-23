be7a412d9f3b7835cf014903afaa75484a864ad3
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-03-23 23:43:26 +0700

    Time: 52 ms (75.88%), Space: 12 MB (76.85%) - LeetHub

---
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -0,0 +1,27 @@
+#pragma GCC optimize("O3, unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,lzcnt,popcnt")
+static const bool __io_boost = [](){
+    cin.tie(nullptr); cout.tie(nullptr);
+    return ios_base::sync_with_stdio(false);      
+}();
+
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        string x = s;
+        reverse(x.begin(), x.end());
+        int dp[n+1][n+1];
+        for(int i = 0; i <= n; i++)
+            for(int j = 0; j <= n; j++)
+                if(i == 0 || j == 0)
+                    dp[i][j] = 0;
+                else{
+                    if(s[i-1] == x[j-1])
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    else
+                        dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                }
+        return dp[n][n];
+    }
+};
