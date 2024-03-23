b4cdecfb12d13d2ab24c8e9f2a8743792934229c
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-03-23 22:28:35 +0700

    Time: 11 ms (83.23%), Space: 27.6 MB (11.42%) - LeetHub

--- 0169-majority-element/0169-majority-element.cpp
+++ 0169-majority-element/0169-majority-element.cpp
@@ -2,6 +2,17 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         sort(nums.begin(), nums.end());
-        return nums[nums.size()/2];
+        int cnt = 1;
+        if(nums.size() == 1)
+            return nums[0];
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i] == nums[i-1])
+                cnt++;
+            else
+                cnt = 1;
+            if(cnt > nums.size()/2)
+                return nums[i];
+        }
+        return 0;
     }
 };
