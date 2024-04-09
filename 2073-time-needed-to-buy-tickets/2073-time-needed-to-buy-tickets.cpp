6c166f247bbb2507050ea8889a9070f443a7d300
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-04-09 15:35:16 +0700

    Time: 0 ms (100.00%), Space: 9.3 MB (67.56%) - LeetHub

---
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int timeRequiredToBuy(vector<int>& tickets, int k) {
+        int ans = 0;
+        for(int i = 0; i < tickets.size(); i++){
+            ans += min(tickets[k], tickets[i]);
+            if(i == k)
+                tickets[k]--;
+        }
+        return ans;
+    }
+};
