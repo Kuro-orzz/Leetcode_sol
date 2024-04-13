03ea71bc230ad35cf78b35e1be8f0a5b9ed1ba18
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-04-13 16:12:08 +0700

    Time: 66 ms (41.55%), Space: 24.5 MB (56.01%) - LeetHub

--- 0221-maximal-square/0221-maximal-square.cpp
+++ 0221-maximal-square/0221-maximal-square.cpp
@@ -3,37 +3,20 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int n = matrix.size();
         int m = matrix[0].size();
-        if(n == 1 || m == 1){
-            for(int i = 0; i < n; i++)
-                for(int j = 0; j < m; j++)
-                    if(matrix[i][j] == '1')
-                        return 1;
-            return 0;
-        }
         int ans = 0;
-        bool kt = true;
-        vector<vector<int>> dp(n, vector<int> (m));
+        vector<vector<int>> dp(n, vector<int>(m));
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++){
                 dp[i][j] = matrix[i][j]-'0';
-                if(dp[i][j] == 1){
-                    ans = 1;
-                    kt = false;
-                }
+                ans = max(ans, dp[i][j]);
             }
-        while(!kt){
-            n--;
-            m--;
-            kt = true;
-            for(int i = 0; i < n; i++)
-                for(int j = 0; j < m; j++){
-                    if(dp[i][j] == dp[i][j+1] && dp[i][j] == dp[i+1][j] && dp[i][j] == dp[i+1][j+1])
-                        dp[i][j]++;
-                    if(kt && dp[i][j] == ans+1){
-                        ans++;
-                        kt = false;
-                    }
-                }
+        
+        for(int i = 1; i < n; i++){
+            for(int j = 1; j < m; j++){
+                if(dp[i][j])
+                    dp[i][j] = min({dp[i-1][j], dp[i][j-1], dp[i-1][j-1]})+1;
+                ans = max(ans, dp[i][j]);
+            }
         }
         return ans*ans;
     }
