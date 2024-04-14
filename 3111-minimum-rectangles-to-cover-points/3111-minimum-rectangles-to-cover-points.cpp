077846b7dd4d613d3fa0bbc74f36d5abaf51c95a
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-04-14 11:34:04 +0700

    Time: 2164 ms (20.00%), Space: 569.4 MB (20.00%) - LeetHub

---
+++ 3111-minimum-rectangles-to-cover-points/3111-minimum-rectangles-to-cover-points.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minRectanglesToCoverPoints(vector<vector<int>>& points, int w) {
+        auto cmp = [&](vector<int> a, vector<int> b){
+            return a[0] < b[0];
+        };
+        sort(points.begin(), points.end(), cmp);
+        int ans = 0;
+        int start = -1, end = -1;
+        for(int i = 0; i < points.size(); i++){
+            if(end < points[i][0]){
+                ans++;
+                start = points[i][0];
+                end = start + w;
+            }
+            else continue;
+        }
+        return ans;
+    }
+};
