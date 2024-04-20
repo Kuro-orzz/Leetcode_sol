d6de30e933711f79e63775e5beb1cc5f65f75b82
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-04-20 20:43:44 +0700

    Time: 93 ms (85.82%), Space: 59.2 MB (97.82%) - LeetHub

--- 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -9,7 +9,6 @@ public:
         for(int i = 0; i < n; i++)
             for(int j = 0; j < m; j++){
                 if(land[i][j] && !visited[i][j]){
-                    cout << i << " " << j << endl;
                     int x = i, y = j;
                     while(x+1 < n && land[x+1][j] && !visited[x+1][y])
                         x++;
