59e79713ee2c4ced84a8a5ab587df0fa2e9e63df
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-04-20 20:41:50 +0700

    Time: 147 ms (40.36%), Space: 59.3 MB (84.36%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<vector<int>> findFarmland(vector<vector<int>>& land) {
+        int n = land.size();
+        int m = land[0].size();
+        vector<vector<int>> ans;
+        bool visited[n][m];
+        memset(visited, false, sizeof visited);
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < m; j++){
+                if(land[i][j] && !visited[i][j]){
+                    cout << i << " " << j << endl;
+                    int x = i, y = j;
+                    while(x+1 < n && land[x+1][j] && !visited[x+1][y])
+                        x++;
+                    while(y+1 < m && land[i][y+1] && !visited[i][y+1])
+                        y++;
+                    for(int k = i; k <= x; k++)
+                        for(int t = j; t <= y; t++)
+                            visited[k][t] = true;
+                    ans.push_back({i, j, x, y});
+                }
+            }
+        return ans;
+    }
+};
