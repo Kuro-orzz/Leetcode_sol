252d02a2eeb0792811f9969af63d46051a2a5231
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-04-29 23:27:45 +0700

    Time: 105 ms (73.47%), Space: 91.4 MB (25.13%) - LeetHub

--- 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
+++ 2997-minimum-number-of-operations-to-make-array-xor-equal-to-k/2997-minimum-number-of-operations-to-make-array-xor-equal-to-k.cpp
@@ -1,22 +1,13 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int k) {
-        vector<int> bit(32, 0);
-        for(int i = 0; i < nums.size(); i++){
-            for(int j = 0; j < 32; j++)
-                if(nums[i] & (1 << j))
-                    bit[j]++;
-        }
+        int tmp = nums[0];
+        for(int i = 1; i < nums.size(); i++)
+            tmp = tmp ^ nums[i];
         int ans = 0;
         for(int i = 0; i < 32; i++){
-            if((k & (1 << i)) == 0){
-                if(bit[i] & 1)
-                    ans++;
-            }
-            else{
-                if(!(bit[i] & 1))
-                    ans++;
-            }
+            if((k & (1 << i)) != (tmp & (1 << i)))
+                ans++;
         }
         return ans;
     }
