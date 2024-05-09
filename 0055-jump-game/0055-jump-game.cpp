4d51b00bc4ab010def10898ccfaa4fddec289c94
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-05-09 17:11:31 +0700

    Time: 39 ms (83.16%), Space: 50.9 MB (50.31%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -4,15 +4,13 @@ public:
         int n = nums.size();
         if(n == 1)
             return true;
-        vector<int> dp(n, INT_MAX);
-        dp[0] = 0;
-        for(int i = 0; i < n; i++){
-            if(dp[i] != INT_MAX){
-                for(int j = i+1; j < min(n, i+nums[i]+1); j++){
-                    dp[j] = min(dp[j], dp[i]+1);
-                }
-            }
+        int t = n-1;
+        for(int i = n-2; i >= 0; i--){
+            if(i + nums[i] >= t)
+                t = i;
+            if(t == 0)
+                return true;
         }
-        return (dp[n-1] == INT_MAX ? 0 : dp[n-1]);
+        return false;
     }
 };
