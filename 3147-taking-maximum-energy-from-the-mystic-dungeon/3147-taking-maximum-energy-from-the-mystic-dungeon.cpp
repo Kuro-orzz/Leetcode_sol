dc6d5f456a88835713e19a7b9cc5aac439846b23
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-05-21 00:18:32 +0700

    Time: 147 ms (50.97%), Space: 143.7 MB (89.73%) - LeetHub

---
+++ 3147-taking-maximum-energy-from-the-mystic-dungeon/3147-taking-maximum-energy-from-the-mystic-dungeon.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int maximumEnergy(vector<int>& energy, int k) {
+        int n = energy.size();
+        for(int i = n-1; i >= 0; i--){
+            if(i+k < n)
+                energy[i] += energy[i+k];
+        }   
+        return *max_element(energy.begin(), energy.end());
+    }
+};
