dcf362c85eb00d1941bfccfc4544d0fc33e5d85c
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-05-25 00:24:41 +0700

    Time: 5 ms (77.28%), Space: 12.4 MB (85.05%) - LeetHub

---
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    map<char, int> mp;
+    map<char, int> used;
+    int ans = 0;
+    void solve(int k, vector<string> &words, vector<int> &score){
+        for(auto it : mp)
+            if(it.second < 0)
+                return;
+        int tmp = 0;
+        for(auto it : used)
+            tmp += score[it.first-'a'] * it.second;
+        ans = max(tmp, ans);
+        for(int i = k; i < words.size(); i++){
+            for(auto it : mp)
+                if(it.second < 0)
+                    return;
+            tmp = 0;
+            for(auto it : used)
+                tmp += score[it.first-'a'] * it.second;
+            ans = max(tmp, ans);
+            for(int j = 0; j < words[i].size(); j++){
+                mp[words[i][j]]--;
+                used[words[i][j]]++;
+            }
+            solve(i+1, words, score);
+            for(int j = 0; j < words[i].size(); j++){
+                mp[words[i][j]]++;
+                used[words[i][j]]--;
+            }
+        }
+    }
+
+    int maxScoreWords(vector<string>& words, vector<char>& letters, vector<int>& score) {
+        for(char c : letters)
+            mp[c]++;
+        solve(0, words, score);
+        return ans;
+    }
+};
