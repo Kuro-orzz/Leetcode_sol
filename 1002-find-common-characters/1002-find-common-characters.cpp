efbe950ae51c6c8c83592648efaeaf4590173616
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-06-05 08:29:09 +0700

    Time: 4 ms (84.24%), Space: 11.4 MB (60.89%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.cpp
+++ 1002-find-common-characters/1002-find-common-characters.cpp
@@ -3,11 +3,11 @@ public:
     vector<string> commonChars(vector<string>& words) {
         vector<int> v(26, INT_MAX);
         for(int i = 0; i < words.size(); i++){
-            unordered_map<char, int> mp;
+            vector<int> t(26, 0);
             for(char c : words[i])
-                mp[c]++;
+                t[c-'a']++;
             for(int j = 0; j < 26; j++)
-                v[j] = min(v[j], mp[char(j+'a')]);
+                v[j] = min(v[j], t[j]);
         }
         vector<string> ans;
         for(int i = 0; i < 26; i++)
