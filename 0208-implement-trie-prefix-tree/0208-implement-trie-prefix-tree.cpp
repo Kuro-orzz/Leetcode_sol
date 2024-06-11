6deb23a319dc5463e26c0061209b23eb204c941c
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-06-11 10:50:12 +0700

    Time: 37 ms (93.83%), Space: 48.2 MB (69.83%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -1,44 +1,47 @@
+struct node{
+    node* child[26];
+    int isEnd;
+    node(){
+        memset(child, 0, sizeof(child));
+        isEnd = 0;
+    }
+};
 class Trie {
 public:
-    int child[300000][26];
-    bool isEnd[300000];
-    int cnt = 0;
+    node* root;
     Trie() {
-        for(int i = 0; i < 300000; i++)
-            memset(child[i], 0, sizeof(child[i]));
-        memset(isEnd, 0, sizeof(isEnd));
-        cnt = 0;
+        root = new node;
     }
     
     void insert(string word) {
-        int u = 0;
+        node* u = root;
         for(int i = 0; i < word.size(); i++){
             int k = word[i]-'a';
-            if(!child[u][k])
-                child[u][k] = ++cnt;
-            u = child[u][k];
+            if(!u->child[k])
+                u->child[k] = new node();
+            u = u->child[k];
         }
-        isEnd[u] = 1;
+        u->isEnd = 1;
     }
     
     bool search(string word) {
-        int u = 0;
+        node* u = root;
         for(int i = 0; i < word.size(); i++){
             int k = word[i]-'a';
-            if(!child[u][k])
+            if(!u->child[k])
                 return false;
-            u = child[u][k];
+            u = u->child[k];
         }
-        return isEnd[u];
+        return u->isEnd;
     }
     
     bool startsWith(string prefix) {
-        int u = 0;
+        node* u = root;
         for(int i = 0; i < prefix.size(); i++){
             int k = prefix[i]-'a';
-            if(!child[u][k])
+            if(!u->child[k])
                 return false;
-            u = child[u][k];
+            u = u->child[k];
         }
         return true;
     }
