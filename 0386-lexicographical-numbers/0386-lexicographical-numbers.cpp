a390a8edc0b918a78e3c49a4ada8dccc518f2a68
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-06-19 22:50:22 +0700

    Time: 31 ms (29.13%), Space: 31.7 MB (5.48%) - LeetHub

--- 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.cpp
@@ -10,7 +10,7 @@ public:
     };
     Node* r = new Node();
     vector<int> ans;
-    void add(string& s){
+    void add(string s){
         Node* u = r;
         for(int i = 0; i < s.size(); i++){
             int k = s[i]-'0';
@@ -20,24 +20,19 @@ public:
         }
         u->isEnd = true;
     }
-    void travel(Node* r, string& s){
+    void travel(Node* r, string s){
         Node* u = r;
         if(u->isEnd) 
             ans.push_back(stoi(s));
         for(int i = 0; i < 10; i++){
-            if(u->child[i]){
-                string x = s + to_string(i);
-                travel(u->child[i], x);
-            }
+            if(u->child[i])
+                travel(u->child[i], s + to_string(i));
         }
     }
     vector<int> lexicalOrder(int n) {
-        for(int i = 1; i <= n; i++){
-            string x = to_string(i);
-            add(x);
-        }
-        string s = "";
-        travel(r, s);      
+        for(int i = 1; i <= n; i++)
+            add(to_string(i));
+        travel(r, "");      
         return ans;
     }
 };
