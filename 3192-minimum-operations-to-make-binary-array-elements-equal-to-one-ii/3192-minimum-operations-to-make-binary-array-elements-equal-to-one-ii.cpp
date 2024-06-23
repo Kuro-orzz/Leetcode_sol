a64ed1ac8a42aff181378867408500492d7e33fa
Kuro-orzz <149597765+Kuro-orzz@users.noreply.github.com>
2024-06-23 17:45:33 +0700

    Time: 124 ms (77.78%), Space: 155 MB (66.67%) - LeetHub

--- 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
+++ 3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii/3192-minimum-operations-to-make-binary-array-elements-equal-to-one-ii.cpp
@@ -3,8 +3,8 @@ public:
     int minOperations(vector<int>& nums) {
         int ans = 0;
         for(int i = 0; i < nums.size(); i++){
-            if(ans % 2 != 0)
-                nums[i] = 1-nums[i];
+            if(ans & 1)
+                nums[i] ^= 1;
             if(nums[i] == 0){
                 nums[i] = 1;
                 ans++;
