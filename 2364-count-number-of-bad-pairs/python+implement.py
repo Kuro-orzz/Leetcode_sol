06dcba3d32c7e1c8e6318d4f2c069606ec0d27e1
Kuro-orzz <23020674@vnu.edu.vn>
2025-02-11 22:27:08 +0700

    update

--- 2364-count-number-of-bad-pairs/python+implement.py
+++ 2364-count-number-of-bad-pairs/python+implement.py
@@ -1,3 +1,15 @@
-for i in range(n):
-            key = nums[i] - i
-            mp[key] = mp.get(key, 0) + 1
+class Solution(object):
+    def countBadPairs(self, nums):
+        n = len(nums)
+        mp = dict()
+        for i in range(n):
+            mp[nums[i]-i] = mp.get(nums[i]-i, 0) + 1
+        ans = n*(n-1)/2
+        for y in mp.values():
+            ans -= y*(y-1)/2
+        return ans
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        
