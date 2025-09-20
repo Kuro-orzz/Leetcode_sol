18c83e27b742086cbb20228ee56712ddcee3ad45
Kuro-orzz <23020674@vnu.edu.vn>
2025-09-20 14:14:37 +0700

    update

--- 3227-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
+++ 3227-vowels-game-in-a-string/3462-vowels-game-in-a-string.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
+    bool check(char c) {
+        return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
+    }
+
     bool doesAliceWin(string s) {
         int cnt = 0;
         vector<int> v;
-        auto check = [&](char c){
-            return c=='a' || c=='e' || c=='i' || c=='o' || c=='u';
-        };
-        for(int i = 0; i < s.size(); i++){
-            if(check(s[i])){
+        for (int i = 0; i < s.size(); i++){
+            if ( check(s[i]) ){
                 cnt++;
                 v.push_back(i);
             }
         }
-        if(cnt == 0) return false;
-        else return true;
+        return cnt;
     }
 };
